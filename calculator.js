27fee47ba2b0cf4eacf8fb2655cf774285428e3f
esraamalkawi <esraamalkawi462@gmail.com>
2021-05-18 01:34:36 -0700

    solved

--- calculator.js
+++ calculator.js
@@ -4,3 +4,30 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+
+let num1 = prompt("please enter the first number:")
+let num2 = prompt("please enter the second number:")
+let operation = prompt("Please choose the operation (+, -, /, *):");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+if (Number.isInteger(num1) && Number.isInteger(num2)) {
+if (operation === "+" ) {
+
+    console.log("that the operation is not valid")
+}    
+else  if (operation === "-") {
+    console.log("that the operation is not valid")
+    
+} else if (operation ==="/") {
+    console.log("that the operation is not valid")
+    
+} else  if (operation === "*") {
+    console.log(num1 * num2)
+}
+    else 
+    console.log ("the numbers were invalid.")
+}
+
+    
+
+     
